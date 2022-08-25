da386dc7fe8f93fd2ff5cff363e480dbe8479173
aishalkandari <aeshah.q8y@gmail.com>
2022-08-25 16:58:17 +0300

    cw9 is done
    done

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // أنشئ المصفوفة تحت هذا الكومنت
+let orders = [];
 
 
 function addOrder() {
@@ -8,16 +9,75 @@ function addOrder() {
   let order = document.getElementById("order").value;
   
   // أنشئ كائن newOrder
-  
+  let newOrder = {
+
+    name: name,
+    id: id,
+    phone: phone,
+    order: order,
+   
+     
+
+  };
   // أدرج الكائن إلى المصفوفة
-  
+  orders.push(newOrder);
   // طبق الخطوة 7 تحت هذا الكومنت
-  
+  load();
+
 }
+
+
 
 function load() {
   let container = document.getElementById("container");
-  
+  container.innerHTML = "";
   // طبّق الخطوة 6 تحت هذا الكومنت
+orders.forEach((item) => {
+
+container.innerHTML += `<div><h1>${item.name} - ${item.id}</h1><h3>${item.order}</h3></div>`;
+
+
+
+
+
+});
+
 
 }
+
+
+
+function removeOrder () {
+  
+  let name = document.getElementById("name").value;
+  let id = document.getElementById("id").value;
+  let phone = document.getElementById("phone").value;
+  let order = document.getElementById("order").value;
+
+  let newOrder = {
+
+    name: name,
+    id: id,
+    phone: phone,
+    order: order,
+   
+     
+
+  };
+  // أدرج الكائن إلى المصفوفة
+  orders.pop(newOrder);
+  // طبق الخطوة 7 تحت هذا الكومنت
+  load();
+
+
+
+
+
+}
+
+
+
+
+
+
+
